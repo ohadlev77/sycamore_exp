59d16cffccd3f64eeedb11418e41a1e69a6c2d7b
Ohad Lev <ohadlev77@gmail.com>
2024-01-31 13:33:18 +0200

    Update jakarta and nairobi data with expected fidelities computed with eq (77)

--- README.txt
+++
@@ -1,4 +0,0 @@
-Need to organize `cirq_impl` vs `qiskit_impl`:
-1. `analyze_data.py` is a duplicate in both directories.
-2. `google_chosen_data` is (more or less) a duplicate in both directories.
-3. There are notebooks in both directories.
--- qiskit_rcs_v2/compute_expected_circuit_fidelity.py
+++ qiskit_rcs_v2/compute_expected_circuit_fidelity.py
@@ -56,3 +56,55 @@ def compute_expected_fidelity(
         fidelity *= 1 - qubit_readout_error["value"]
         
     return {"expected_fidelity_before_readout": fidelity_before_readout, "expected_fidelity": fidelity}
+
+
+def compute_expected_fidelity_from_csv(
+    qc: QuantumCircuit,
+    backend_calibrations_csv_file_path: str
+) -> Dict[str, float]:
+    """Calculates a total expected fidelity for a given quantum circuit `qc`, according
+    to characterization data in the file at `backend_calibrations_csv_file_path` - by multiplying
+    all gate-fidelties of the gates in `qc` with measurement-fidelities of the qubits in `qc.
+    
+    Returns:
+        (Dict[str, float]): the calculated expected fidelity before taking
+        readout errors into account, and after."""
+
+    
+    calib_df = pd.read_csv(backend_calibrations_csv_file_path)
+    noisy_instructions = ["sx", "x", "cx"]
+        
+    fidelity = 1
+    for instruction in qc.data:
+        inst_name = instruction.operation.name
+        
+        if inst_name in noisy_instructions:
+            inst_qubits = [qubit.index for qubit in instruction.qubits]
+            gate_error_data = calib_df.loc[inst_qubits[0], f"{inst_name}_error"]
+
+            if inst_name == "cx":
+                gates_errors = gate_error_data.replace(" ", "").split(";")
+                gates_errors = [gate_error.split(":") for gate_error in gates_errors]
+
+                for gate_error in gates_errors:
+                    gate_error[0] = gate_error[0].split("_")
+
+                for gate_error in gates_errors:
+                    if str(inst_qubits[0]) in gate_error[0] and str(inst_qubits[1]) in gate_error[0]:
+                        gate_fidelity = 1 - float(gate_error[1])     
+
+            else:
+                gate_fidelity = 1 - gate_error_data
+            
+            fidelity *= gate_fidelity
+    fidelity_before_readout = fidelity
+
+    dag_qc = circuit_to_dag(qc)
+    active_qubits = [qubit.index for qubit in qc.qubits if qubit not in dag_qc.idle_wires()]
+
+    for qubit_index in active_qubits:
+        qubit_readout_error = calib_df.loc[qubit_index, "readout_assignment_error"]
+        
+        fidelity *= 1 - qubit_readout_error
+        
+    return {"expected_fidelity_before_readout": fidelity_before_readout, "expected_fidelity": fidelity}
